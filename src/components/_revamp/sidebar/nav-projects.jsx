faec2e72a0a122caf45f41a5cf615f8a920ea203
Alexander William <alexwilliam112@gmail.com>
2025-06-21 22:21:03 +0700

    feat: added my tasks

--- src/components/_revamp/sidebar/nav-projects.jsx
+++ src/components/_revamp/sidebar/nav-projects.jsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import { MoreHorizontal } from 'lucide-react'
+import { MoreHorizontal } from "lucide-react";
 
 import {
   SidebarGroup,
@@ -8,22 +8,42 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from '@/components/ui/sidebar'
-import { Bell } from 'lucide-react'
-import Link from 'next/link'
-import { ListTodo } from 'lucide-react'
+} from "@/components/ui/sidebar";
+import { Bell } from "lucide-react";
+import Link from "next/link";
+import { ListTodo } from "lucide-react";
+import { useUserStore } from "@/store/user/userStore";
+import { useEffect } from "react";
 
 export function NavProjects() {
+  const userId = useUserStore((state) => state.user_id);
+
+  const navigateTo = (page, paramId) => {
+    const params = new URLSearchParams(window.location.search);
+    const workspaceId = params.get("workspace_id");
+
+    if (workspaceId) {
+      const url = `/dashboard?workspace_id=${workspaceId}&page=${page}&param_id=${paramId}`;
+      window.location.href = url; // Navigate to the constructed URL
+    } else {
+      console.error("workspace_id is missing in the query parameters.");
+    }
+  };
+
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:block">
       <SidebarGroupLabel>My Workspace</SidebarGroupLabel>
       <SidebarMenu>
         <SidebarMenuItem>
           <SidebarMenuButton asChild tooltip="My Tasks">
-            <Link href="/notifications">
+            <a
+              href="#"
+              title={"My Tasks"}
+              onClick={() => navigateTo("my_tasks", userId)} // Navigate to team
+            >
               <ListTodo />
               <span>My Tasks</span>
-            </Link>
+            </a>
           </SidebarMenuButton>
         </SidebarMenuItem>
 
@@ -37,5 +57,5 @@ export function NavProjects() {
         </SidebarMenuItem>
       </SidebarMenu>
     </SidebarGroup>
-  )
+  );
 }
