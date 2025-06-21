53ba5fa2cd9fab297851c400469b1ec041063ac9
Abdullah <abdul.abdullah@mekari.com>
2025-06-21 14:17:25 +0700

    feat: notification service

--- src/service/api/api-client.mjs
+++ src/service/api/api-client.mjs
@@ -12,4 +12,22 @@ const apiClient = axios.create({
   },
 })
 
-export { apiClient }
+const apiClientV2 = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+apiClientV2.interceptors.response.use(function (response) {
+  const { data, error, message } = response.data
+
+  if (error) return Promise.reject(message)
+
+  response.data = data
+
+  return response
+})
+
+export { apiClient, apiClientV2 }
