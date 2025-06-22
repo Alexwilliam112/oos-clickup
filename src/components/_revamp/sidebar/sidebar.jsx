8badb352a90dc26175e61fc01a7e801199be6255
Abdullah <abdul.abdullah@mekari.com>
2025-06-22 13:07:57 +0700

    feat: avatar and title

--- src/components/_revamp/sidebar/sidebar.jsx
+++ src/components/_revamp/sidebar/sidebar.jsx
@@ -15,6 +15,7 @@ import { useQuery } from '@tanstack/react-query'
 import { workspaceService } from '@/service/index.mjs'
 import { useSearchParams } from 'next/navigation'
 import { Fragment } from 'react'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 
 export default function Sidebar({ children }) {
   const params = useSearchParams()
@@ -32,20 +33,40 @@ export default function Sidebar({ children }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-b">
+        <header className="flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-b">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-6" />
+            <div className="flex items-center gap-2 min-w-36">
+              <Avatar className="h-9 w-9 bg-white-500 text-white">
+                <AvatarFallback className="bg-purple-700 text-sm">
+                  {data?.title.slice(0, 2).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+
+              <h1 className="text-base sm:text-lg font-medium">{data?.title}</h1>
+            </div>
+
+            <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-6" />
+
             <Breadcrumb>
               <BreadcrumbList>
                 {data?.path?.map((path, index) => (
                   <Fragment key={index}>
                     <BreadcrumbItem className="hidden md:block">
-                      <BreadcrumbLink
-                        href={`/dashboard?workspace_id=${workspace_id}&page=${path.page}&param_id=${path.id}`}
-                      >
-                        {path.name}
-                      </BreadcrumbLink>
+                      {index + 1 === data?.path.length ? (
+                        <BreadcrumbPage
+                          href={`/dashboard?workspace_id=${workspace_id}&page=${path.page}&param_id=${path.id}`}
+                        >
+                          {path.name}
+                        </BreadcrumbPage>
+                      ) : (
+                        <BreadcrumbLink
+                          href={`/dashboard?workspace_id=${workspace_id}&page=${path.page}&param_id=${path.id}`}
+                        >
+                          {path.name}
+                        </BreadcrumbLink>
+                      )}
                     </BreadcrumbItem>
                     {index + 1 < data.path.length && (
                       <BreadcrumbSeparator className="hidden md:block" />
